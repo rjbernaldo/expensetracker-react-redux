dd0d1022cba70a3504d131c7d48a825be574b30b
Rj Bernaldo <rj.bernaldo@gmail.com>
2017-04-03 09:21:27 +0800

    Remove kube and add react touch events

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
 require('./lib/whatwg-fetch/fetch.js')
-require('./lib/kube-6.0.1/kube.min.css')
+//require('./lib/kube-6.0.1/kube.min.css')
+
+// Needed for onTouchTap
+// http://stackoverflow.com/a/34015469/988941
+import injectTapEventPlugin from 'react-tap-event-plugin';
+injectTapEventPlugin();
 
 import 'react-hot-loader/patch'
 import React from 'react'
